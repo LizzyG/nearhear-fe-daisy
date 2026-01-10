59033420021b6049f2480b43eb85ed7e4480d255
LizzyG <liz.gabriel@gmail.com>
2026-01-10 13:43:06 -0800

    dont' cover mailing list popup with map

--- src/components/MailingListModal.vue
+++ src/components/MailingListModal.vue
@@ -142,7 +142,7 @@ onUnmounted(() => {
   <Teleport to="body">
     <div
       v-if="isOpen"
-      class="fixed inset-0 z-50 flex items-center justify-center bg-black/50 p-4"
+      class="fixed inset-0 z-[9999] flex items-center justify-center bg-black/50 p-4"
       @click.self="dismissModal"
     >
       <div
